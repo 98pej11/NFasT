a98060de0639d5818b2aaac2b01a17592bf05eee
박소정 <sjcoder9611@naver.com>
2023-03-12 23:50:11 +0900

    * FEAT : Add NFast Card and Header
    Consist of card and floating button and header
    
    RESOLVES : #

--- frontend/nfast/src/App.jsx
+++ frontend/nfast/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/commons/Header";
+// import NFastCard from "./components/commons/NFastCard";
+// import NFastForUse from "./components/NFastForUse";
 // import Header from "./components/commons/Header";
-import NfastCard from "./components/NfastCard";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/" element={<NfastCard />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
