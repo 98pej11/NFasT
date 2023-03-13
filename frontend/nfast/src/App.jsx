a6c4ce3aae751372fe8e4325ad391e50084ddc16
박소정 <sjcoder9611@naver.com>
2023-03-13 11:11:34 +0900

    * FEAT : fix uninstall daisy

--- frontend/nfast/src/App.jsx
+++ frontend/nfast/src/App.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/commons/Header";
-// import NFastCard from "./components/commons/NFastCard";
-// import NFastForUse from "./components/NFastForUse";
-// import Header from "./components/commons/Header";
-import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </BrowserRouter>
+      <h1>하이</h1>
     </div>
   );
 }
