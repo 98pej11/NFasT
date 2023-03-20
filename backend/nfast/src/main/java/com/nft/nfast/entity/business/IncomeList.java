651852a1a79a69d7e21355d8499c3bd5e0e5e8b9
duridudu <acd1026@naver.com>
2023-03-20 16:09:25 +0900

    📝 UPDATE : develop 병합 최종(이어야 함)

---
+++ backend/nfast/src/main/java/com/nft/nfast/entity/business/IncomeList.java
@@ -0,0 +1,56 @@
+package com.nft.nfast.entity.business;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Builder
+public class IncomeList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long incomeListSequence;
+
+    @Column(nullable = false)
+    private String incomeListTransaction;
+
+    @Column(nullable = false)
+    private BigDecimal incomeListPrice;
+
+    @Column(nullable = false)
+    private Date incomeListDate;
+
+    @Column(nullable = false)
+    private Byte incomeListType;
+
+    @Column(nullable = false)
+    private long storeSequence;
+
+    @Builder
+    public IncomeList(long incomeListSequence, String incomeListTransaction, BigDecimal incomeListPrice, Date incomeListDate, Byte incomeListType, long storeSequence) {
+        this.incomeListSequence = incomeListSequence;
+        this.incomeListTransaction=incomeListTransaction;
+        this.incomeListPrice = incomeListPrice;
+        this.incomeListDate = incomeListDate;
+        this.incomeListType = incomeListType;
+        this.storeSequence = storeSequence;
+    }
+
+//    public IncomelistDto toDto(){
+//        IncomelistDto incomelistDto = IncomelistDto.builder()
+//                .incomeListSequence(incomeListSequence)
+//                .incomeListTransaction(incomeListTransaction)
+//                .incomeListPrice(incomeListPrice)
+//                .incomeListDate(incomeListDate)
+//                .incomeListType(incomeListType)
+//                .storeSequence(storeSequence)
+//                .build();
+//        return incomelistDto;
+//    }
+}
--- backend/nfast/src/main/java/com/nft/nfast/repository/NfastRepository.java
+++ backend/nfast/src/main/java/com/nft/nfast/repository/NfastRepository.java
@@ -1,15 +1,16 @@
 package com.nft.nfast.repository;
 
 import com.nft.nfast.entity.business.Nfast;
+import com.nft.nfast.model.dto.business.NfastMintedDto;
 import com.nft.nfast.model.dto.business.NfastPurchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface NfastRepository extends JpaRepository<Nfast,Long> {
@@ -21,7 +22,12 @@ public interface NfastRepository extends JpaRepository<Nfast,Long> {
     @Query(value="select nfast_date as nfastDate, count(*) as amount, nfast_price as price from nfast where nfast_date=?1 group by nfast_price", nativeQuery = true)
     List<NfastPurchase> findAllByNfastDate(String nfastDate);
 
+    @Query(value=
+            "select nfast_date as nfastDate, nfast_price as nfastPrice, count(case when nfast_sale_state=0 then 1 end) as nfastSaleCount, count(*) as nfastTotalCount from nfast where store_sequence=:store group by nfast_date ", nativeQuery = true)
+    List<NfastMintedDto> findUsedByNfastDate(@Param("store") Long store);
+//    count(case when nfast_sale_state=0 then 1 end) as nfastSaleCount, count(*) as nfastTotalCount
+
     @Query(value="select * from nfast where nfast_sale_state!=2 and store_sequence=?1 and nfast_date=?2 and nfast_price=?3 limit ?4", nativeQuery = true)
-    List<Nfast> findTopAmountNfastByParam(long storeSequence, String nfastDate,BigDecimal nfastPrice, int amount);
+    List<Nfast> findTopAmountNfastByParam(long storeSequence, String nfastDate, BigDecimal nfastPrice, int amount);
 }
 
