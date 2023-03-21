712509f36c2a719582bb32c665198731405ecbbe
박은정 <98pej11@gmail.com>
2023-03-21 17:56:32 +0900

    ⭐ FEAT : 사장님 소개글 UI 구현

--- frontend/nfast/src/pages/seller/IntroducePage.jsx
+++ frontend/nfast/src/pages/seller/IntroducePage.jsx
@@ -1,35 +1,92 @@
-import React from "react";
-// import { autoPlay, virtualize } from "react-swipeable-views-utils";
-// import SwipeableViews from "react-swipeable-views";
-import Intro1 from "../../components/intropage/Intro1";
-// import Intro2 from "../../components/intropage/Intro2";
-// import Intro3 from "../../components/intropage/Intro3";
+import React, { useRef, useEffect, useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import ProgressBar from "../../components/commons/ProgressBar";
+import {
+  Intro1,
+  Intro2,
+  Intro3,
+  Intro4,
+  Intro5,
+  Intro6,
+} from "../../components/intropage";
 
-// const VirtualizeSwipeableViews = virtualize(SwipeableViews);
-// const AutoPlaySwipeableViews = autoPlay(VirtualizeSwipeableViews);
-
-function IntroducePage() {
-  //   // eslint-disable-next-line no-unused-vars
-  //   const [index, setIndex] = React.useState(0);
-
-  //   // eslint-disable-next-line no-unused-vars
-  //   const handleChangeIndex = (index) => {
-  //     setIndex(index);
-  //   };
+export default function IntroducePage() {
+  const OuterPageBoxRef = useRef();
+  // const [showTopBtn, setShowTopBtn] = useState(false);
+  useEffect(() => {
+    // eslint-disable-next-line no-unused-vars
+    // const wheelHandler = (e) => {
+    //   const { scrollTop } = OuterPageBoxRef.current;
+    //   if (scrollTop > 0) {
+    //     setShowTopBtn(true);
+    //   } else {
+    //     setShowTopBtn(false);
+    //   }
+    // };
+    const boxRefCurrent = OuterPageBoxRef.current;
+    boxRefCurrent.addEventListener("scroll", wheelHandler);
+    return () => {
+      boxRefCurrent.removeEventListener("scroll", wheelHandler);
+    };
+  }, []);
+  const handleClick = () => {
+    console.log("clicked");
+    OuterPageBoxRef.current.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    // <AutoPlaySwipeableViews onChangeIndex={handleChangeIndex}>
-    <div>
-      <div>
-        <Intro1 />{" "}
-      </div>
-      {/* <div>
-        <Intro2 />{" "}
-      </div> */}
-      {/* <div>
-        <Intro3 />{" "}
-      </div> */}
-    </div>
+    <>
+      <GlobalStyles />
+      <OuterPageBox ref={OuterPageBoxRef}>
+        <ProgressBar OuterPageBoxRef={OuterPageBoxRef} />
+        <Intro1 />
+        <Intro2 />
+        <Intro3 />
+        <Intro4 />
+        <Intro5 />
+        <Intro6 />
+        {/* <ContentPageBox id="contents">
+          <SideBar />
+          <ContentsWrapper>
+            <Skill />
+            <Project />
+            <Education />
+            <Experience />
+            <Extra />
+          </ContentsWrapper>
+        </ContentPageBox> */}
+        {/* {showTopBtn && <MovePageBtn handleClick={handleClick} />} */}
+      </OuterPageBox>
+    </>
   );
 }
 
-export default IntroducePage;
+const GlobalStyles = createGlobalStyle`
+  html {
+    --color-main: #BCAFD4;
+    --color-main-dark: #6C509F;
+    --color-back: #efefef;
+    --color-back-light: #F9F9F9;
+    --color-back-dark: #DCDCDC;
+    --color-detail-back : #f4f4f4;
+    a {
+      color: var(--color-main-dark);
+      text-decoration-line: none;
+    }
+  }
+`;
+const OuterPageBox = styled.div`
+  height: 100vh;
+  overflow-y: auto;
+`;
+
+// const ContentPageBox = styled.div`
+//   display: grid;
+//   grid-template-columns: 1fr 3fr;
+
+//   @media screen and (max-width: 682px) {
+//     grid-template-columns: 1fr;
+//   }
+// `;
