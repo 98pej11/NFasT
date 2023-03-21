cc06eabdcaa81e4fd207ef87ae30c3b590061822
김형균 <heng2926@naver.com>
2023-03-21 21:05:13 +0900

    ✏ DOCS : [SC]NFT 인자값 받기
    ipfs에 업로드할 데이터를 객체 형태로 파일에 저장하여 업로드 하게 하였으며 리턴값 중 cid 값을 사용할 수 있게 빼두었습니다.
    
    RESOLVES : #S08P22A307-194

--- IPFS 학습/my-app/src/App.js
+++ IPFS 학습/my-app/src/App.js
@@ -29,14 +29,19 @@ const App = () => {
     txReceipt: ''
   })
 
-  const captureFile = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
-    const file = event.target.files[0];
-    let reader = new window.FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onloadend = () => convertToBuffer(reader);
+  const captureText = (event) => {
+    setInfo({ buffer : event.target.value});
+    console.log(info.buffer);
   };
+  // const captureFile = (event) => {
+  //   event.stopPropagation();
+  //   event.preventDefault();
+  //   const file = event.target.files[0];
+  //   let reader = new window.FileReader();
+  //   reader.readAsArrayBuffer(file);
+  //   reader.onloadend = () => convertToBuffer(reader);
+  // };
+  
 
   const convertToBuffer = async (reader) => {
     const buffer = await Buffer.from(reader.result);
@@ -78,6 +83,41 @@ const App = () => {
     }
   }
 
+  // const onClick = async () => {
+  //   let table = document.getElementById('TransactionTable');
+
+  //   try {
+  //     setInfo({ blockNumber: "waiting.." });
+  //     setInfo({ gasUsed: "waiting..." });
+
+  //     await web3.eth.getTransactionReceipt(info.transactionHash, (err, txReceipt) => {
+  //       console.log(err, txReceipt);
+  //       setInfo({ txReceipt });
+  //       setInfo({ blockNumber: txReceipt.blockNumber });
+  //       setInfo({ gasUsed: txReceipt.gasUsed });
+  //       let row = `            
+  //         <tr>
+  //           <td>Tx Hash</td>
+  //           <td>${txReceipt.transactionHash}</td>
+  //         </tr>
+
+  //         <tr>
+  //           <td>Block Number</td>
+  //           <td>${txReceipt.blockNumber}</td>
+  //         </tr>
+
+  //         <tr>
+  //           <td>사용 된 가스</td>
+  //           <td>${txReceipt.gasUsed}</td>
+  //         </tr>`
+  //       table.innerHTML += row;
+  //     });
+  //   }
+  //   catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
   const onSubmit = async (event) => {
     let table = document.getElementById('TransactionTable');
     event.preventDefault();
@@ -103,34 +143,105 @@ const App = () => {
       </tr>
     `
     table.innerHTML += row;
+    // await ipfs.add(info.buffer, (err, ipfsHash) => {
+    //   console.log(err);
+    //   console.log(ipfsHash);
+    //   setInfo({ ipfsHash: ipfsHash[0].hash });
+
+    //   storehash.methods.sendHash(ipfsHash[0].hash).send({
+    //     from: accounts[0]
+    //   }, (error, transactionHash) => {
+    //     setInfo({ transactionHash: transactionHash });
+    //     let row = `
+    //     <tr>
+    //       <td>Eth 계약에 저장된 IPFS 해시 번호</td>
+    //       <td>${ipfsHash[0].hash}</td>
+    //     </tr>
+    //     <tr>
+    //       <td>Ethereum 계약 주소</td>
+    //       <td>${ethAddress}</td>
+    //     </tr>
+    //     `
+    //     table.innerHTML += row;
+    //   });
+    // });
   };
+  // const onSubmit = async (event) => {
+  //   let table = document.getElementById('TransactionTable');
+  //   event.preventDefault();
+  //   const accounts = await web3.eth.getAccounts();
+  //   console.log('Sending from Metamask account: ' + accounts[0]);
+  //   const ethAddress = await storehash.options.address;
+  //   console.log("test");
+  //   console.log(info.buffer);
+  //   const file = {
+  //     path: '/tmp/myfile.txt',
+  //     content: info.buffer
+  //   }
+  //   const test = await ipfs.add(file);
+  //   console.log(test.cid)
+  //   let row = `
+  //     <tr>
+  //       <td>Eth 계약에 저장된 IPFS 해시 번호</td>
+  //       <td>${test.cid}</td>
+  //     </tr>
+  //     <tr>
+  //       <td>Ethereum 계약 주소</td>
+  //       <td>${ethAddress}</td>
+  //     </tr>
+  //   `
+  //   table.innerHTML += row;
+  //   // await ipfs.add(info.buffer, (err, ipfsHash) => {
+  //   //   console.log(err);
+  //   //   console.log(ipfsHash);
+  //   //   setInfo({ ipfsHash: ipfsHash[0].hash });
+
+  //   //   storehash.methods.sendHash(ipfsHash[0].hash).send({
+  //   //     from: accounts[0]
+  //   //   }, (error, transactionHash) => {
+  //   //     setInfo({ transactionHash: transactionHash });
+  //   //     let row = `
+  //   //     <tr>
+  //   //       <td>Eth 계약에 저장된 IPFS 해시 번호</td>
+  //   //       <td>${ipfsHash[0].hash}</td>
+  //   //     </tr>
+  //   //     <tr>
+  //   //       <td>Ethereum 계약 주소</td>
+  //   //       <td>${ethAddress}</td>
+  //   //     </tr>
+  //   //     `
+  //   //     table.innerHTML += row;
+  //   //   });
+  //   // });
+  // };
 
   const onChangeValue = async () => {
     let desc = document.getElementById('description').value;
     let img = document.getElementById('image').value;
     let name = document.getElementById('name').value;
-
+    console.log(desc);
+    console.log(img);
+    console.log(name);
     setInput({
-      'description': desc,
-      'external_url': '',
-      'image': img,
-      'name': name,
+      'description': desc, //이용날짜
+      'external_url': '', //사장님 데이터 ipfs add 리턴값
+      'image': img, //수량
+      'name': name, //초기가격
       'attribute': [],
     });
-
-    jsonSubmit();
+    jsonSubmit(); 
+    console.log(input.external_url);
+    // REST API로 DB 단에 데이터 또한 보내야한다.(input에 데이터 넣게 해뒀음)
   }
 
-  const jsonSubmit = () => {
-    ipfs.files.add(Buffer.from(JSON.stringify(input)))
-      .then(res => {
-        const hash = res[0].hash
-        console.log('added data hash:', hash)
-        return ipfs.files.cat(hash)
-      })
-      .then(output => {
-        console.log('retrieved data:', JSON.parse(output))
-      })
+  const jsonSubmit = async () => {
+
+    const file = {
+      path: '/tmp/myfile.txt',
+      content: JSON.stringify(input)
+    }
+    const testc = await ipfs.add(file);
+      setInput( {external_url : testc.cid.string} )
   }
 
   return (
@@ -140,9 +251,14 @@ const App = () => {
         <h3> Choose file to send to IPFS </h3>
         <Form>
           <input
+            type="text"
+            onChange={captureText}
+          />
+
+          {/* <input
             type="file"
             onChange={captureFile}
-          />
+          /> */}
           <Button
             bsStyle="primary"
             onClick={onSubmit}
@@ -167,10 +283,10 @@ const App = () => {
         <div>
           <p>------------------------------------------</p>
           <p>NFT 세부정보 담기</p>
-          <input type="text" placeholder="description" id="description" /><br />
-          <input type="text" placeholder="image url" id="image" /><br />
-          <input type="text" placeholder="name" id="name" /><br />
-          <button onClick={onChangeValue}>제출하기</button>
+          <input type="text" placeholder="지갑주소" id="description" /><br />
+          <input type="text" placeholder="이용 시간" id="image" /><br />
+          <input type="text" placeholder="발행량" id="name" /><br />
+          <button onClick={onChangeValue}>발행하기</button>
         </div>
       </Container >
     </div>
