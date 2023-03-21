efa53a6746d5c4508821da718e7856bb1f01af62
박은정 <eunjung@bag-eunjeong-ui-MacBookPro.local>
2023-03-21 21:47:57 +0900

    📝 UPDATE : 사장님 소개글 컴포넌트 UI 구현

--- frontend/nfast/src/pages/seller/IntroducePage.jsx
+++ frontend/nfast/src/pages/seller/IntroducePage.jsx
@@ -1,92 +1,20 @@
-import React, { useRef, useEffect, useState } from "react";
-import styled, { createGlobalStyle } from "styled-components";
-import ProgressBar from "../../components/commons/ProgressBar";
-import {
-  Intro1,
-  Intro2,
-  Intro3,
-  Intro4,
-  Intro5,
-  Intro6,
-} from "../../components/intropage";
+import React from "react";
+// import styled from "styled-components";
+// import HeaderSeller from "../../components/commons/HeaderSeller";
+// import NFastCard1 from "../../components/mypage/MyNftCard1";
+import Intro1 from "../../components/intropage/Intro1";
+// import Intro2 from "../../components/intropage/Intro2";
+// import Intro3 from "../../components/intropage/Intro3";
+// import Intro4 from "../../components/intropage/Intro4";
+// import Intro5 from "../../components/intropage/Intro5";
+// import Intro6 from "../../components/intropage/Intro6";
 
-export default function IntroducePage() {
-  const OuterPageBoxRef = useRef();
-  // const [showTopBtn, setShowTopBtn] = useState(false);
-  useEffect(() => {
-    // eslint-disable-next-line no-unused-vars
-    // const wheelHandler = (e) => {
-    //   const { scrollTop } = OuterPageBoxRef.current;
-    //   if (scrollTop > 0) {
-    //     setShowTopBtn(true);
-    //   } else {
-    //     setShowTopBtn(false);
-    //   }
-    // };
-    const boxRefCurrent = OuterPageBoxRef.current;
-    boxRefCurrent.addEventListener("scroll", wheelHandler);
-    return () => {
-      boxRefCurrent.removeEventListener("scroll", wheelHandler);
-    };
-  }, []);
-  const handleClick = () => {
-    console.log("clicked");
-    OuterPageBoxRef.current.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+function CurrentIncome() {
   return (
-    <>
-      <GlobalStyles />
-      <OuterPageBox ref={OuterPageBoxRef}>
-        <ProgressBar OuterPageBoxRef={OuterPageBoxRef} />
-        <Intro1 />
-        <Intro2 />
-        <Intro3 />
-        <Intro4 />
-        <Intro5 />
-        <Intro6 />
-        {/* <ContentPageBox id="contents">
-          <SideBar />
-          <ContentsWrapper>
-            <Skill />
-            <Project />
-            <Education />
-            <Experience />
-            <Extra />
-          </ContentsWrapper>
-        </ContentPageBox> */}
-        {/* {showTopBtn && <MovePageBtn handleClick={handleClick} />} */}
-      </OuterPageBox>
-    </>
+    <div>
+      <Intro1 />
+    </div>
   );
 }
 
-const GlobalStyles = createGlobalStyle`
-  html {
-    --color-main: #BCAFD4;
-    --color-main-dark: #6C509F;
-    --color-back: #efefef;
-    --color-back-light: #F9F9F9;
-    --color-back-dark: #DCDCDC;
-    --color-detail-back : #f4f4f4;
-    a {
-      color: var(--color-main-dark);
-      text-decoration-line: none;
-    }
-  }
-`;
-const OuterPageBox = styled.div`
-  height: 100vh;
-  overflow-y: auto;
-`;
-
-// const ContentPageBox = styled.div`
-//   display: grid;
-//   grid-template-columns: 1fr 3fr;
-
-//   @media screen and (max-width: 682px) {
-//     grid-template-columns: 1fr;
-//   }
-// `;
+export default CurrentIncome;
