b8f7c9241d27afedb3824c16191c43e1d2f6f4ae
박소정 <sjcoder9611@naver.com>
2023-03-24 17:52:27 +0900

    � BUILD : 형균이의 ipfs.js 코드 변경
    axios 폴더 안에 ipfs.js 코드
    
    RESOLVES:

--- frontend/nfast/src/components/axios/ipfs.js
+++ frontend/nfast/src/components/axios/ipfs.js
@@ -2,11 +2,60 @@
 // using the infura.io node, otherwise ipfs requires you to run a daemon on your own computer/server. See IPFS.io docs
 // const ipfsClient = require('ipfs-http-client');
 // const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
-// import client from "ipfs-http-client/dist/src";
-const { create } = require("ipfs-http-client");
+// const ipfsClient = require("ipfs-http-client");
+import { Buffer } from "buffer";
 
-const ipfs = create({ url: "http://j8a307.p.ssafy.io:8082/api/v0" });
+const ipfsClient = require("ipfs-http-client");
 
+// const ipfs = create(new URL("https://j8a307.p.ssafy.io/api/v0"));
+
+// const auth = `Basic ${Buffer.from(
+//   `hyueongkyun:17d0a575c0f5483780a2505dd61ce588`
+// ).toString("base64")}`;
+// const ipfs = ipfsClient.create({
+//   host: "ipfs.infura.io",
+//   port: 5001,
+//   protocol: "https",
+//   headers: {
+//     authorization: auth,
+//   },
+// });
+const INFURA_ID = "hyueongkyun";
+const INFURA_SECRET_KEY = "17d0a575c0f5483780a2505dd61ce588";
+const auth = `Basic ${Buffer.from(`${INFURA_ID}:${INFURA_SECRET_KEY}`).toString(
+  "base64"
+)}`;
+const ipfs = ipfsClient.create({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+  headers: {
+    authorization: auth,
+  },
+});
+// const ipfs = ipfsClient.create({
+//   host: "ipfs.infura.io",
+//   port: 5001,
+//   protocol: "https",
+//   headers: {
+//     authorization: `Basic ${Buffer.from(
+//       "2NRlz9rNsZgRLfSdWqrwt7z1TZP:772c0b2f653e8fc42a839654f2730c2f"
+//     ).toString("base64")}`,
+//     "content-type": "multipart/form-data",
+//   },
+// headers: {
+//   "Access-Control-Allow-Origin": "*",
+// },
+// host: "j8a307.p.ssafy.io",
+// // port: "3000",
+// protocol: "https",
+// headers: {
+//   "Access-Control-Allow-Origin": "*",
+//   "Access-Control-Allow-Methods": "*",
+//   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+// },
+// apiPath: "/ipfs",
+// });
 // run with local daemon
 // const ipfsApi = require('ipfs-api');
 // const ipfs = new ipfsApi('localhost', '5001', {protocol: 'http'});
