fceb90b92f267deb76588818da51b4dede9f873a
박은정 <98pej11@gmail.com>
2023-04-01 21:13:29 +0900

    ⭐ FEAT : 거리순 거래순 페이지 구현

--- frontend/nfast/src/pages/customer/MainPage.jsx
+++ frontend/nfast/src/pages/customer/MainPage.jsx
@@ -1,26 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import DepartureBoardTwoToneIcon from "@mui/icons-material/DepartureBoardTwoTone";
 import RecordVoiceOverTwoToneIcon from "@mui/icons-material/RecordVoiceOverTwoTone";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+
 import DistanceCard from "../../components/mainpage/DistanceCard";
 import TransCard from "../../components/mainpage/TransCard";
 import FloatingBtn from "../../components/commons/FloatingBtn";
 import { getAccessToken } from "../../storage/Cookie";
 import { mainAction } from "../../redux/actions/mainAction";
-
-export class Token {
-  #accessToken = "";
-
-  getAccessToken() {
-    return this.#accessToken;
-  }
-
-  setAccessToken(token) {
-    this.#accessToken = token;
-  }
-}
 
 function MainPage() {
   const dispatch = useDispatch();
@@ -55,57 +45,68 @@ function MainPage() {
 
   return (
     <div>
-      <Line>
-        <Title>
-          <DepartureBoardTwoToneIcon />
-          <p>거리순</p>
-        </Title>
-        <span
-          style={{
-            textAlign: "center",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          가까운 거리의 맛집들을 빠르게 만나보세요.
-        </span>
-        <span style={{ float: "right", display: "flex", alignItems: "center" }}>
-          전체 보기 <KeyboardDoubleArrowRightIcon />
-        </span>
-      </Line>
-      <Card>
-        <DistanceCard distanceList={distanceList} />
-      </Card>
-      <div style={{ marginBottom: 100 }}> </div>
-      <Line>
-        <Title>
-          <RecordVoiceOverTwoToneIcon />
-          <p>거래순</p>
-        </Title>
-        <span
-          style={{
-            textAlign: "center",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          최신 가장 많은 거래가 있는 맛집들을 만나보세요.
-        </span>
-        <span style={{ float: "right", display: "flex", alignItems: "center" }}>
-          전체 보기 <KeyboardDoubleArrowRightIcon />
-        </span>
-      </Line>
-      <Card>
-        <TransCard transList={transList} />
-      </Card>
+      <Section>
+        <div style={{ marginTop: 100 }} />
+        <Line>
+          <Title>
+            <DepartureBoardTwoToneIcon />
+            <p>거리순</p>
+          </Title>
+          <Sub>
+            <span>가까운 거리의 맛집들을 빠르게!</span>
+            <Link to="/disall">
+              <span
+                style={{
+                  float: "right",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                전체 보기 <KeyboardDoubleArrowRightIcon />
+              </span>
+            </Link>
+          </Sub>
+        </Line>
+        <Card>
+          <DistanceCard distanceList={distanceList} />
+        </Card>
+      </Section>
+      <Section>
+        <Line>
+          <Title>
+            <RecordVoiceOverTwoToneIcon />
+            <p>거래순</p>
+          </Title>
+          <Sub>
+            <span>최신 많은 거래가 있는 맛집들을 빠르게!</span>
+            <Link to="/transall">
+              <span
+                style={{
+                  float: "right",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                전체 보기 <KeyboardDoubleArrowRightIcon />
+              </span>
+            </Link>
+          </Sub>
+        </Line>
+        <Card>
+          <TransCard transList={transList} />
+        </Card>
+      </Section>
       <FloatingBtn>Floating</FloatingBtn>
-      <div style={{ marginBottom: 100 }}> </div>
     </div>
   );
 }
 
 export default MainPage;
 
+const Section = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
 const Line = styled.div`
   margin-left: 3%;
   margin-right: 3%;
@@ -120,8 +121,14 @@ const Line = styled.div`
 const Card = styled.div`
   margin: 3%;
 `;
+const Sub = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const Title = styled.div`
   display: flex;
-  justify-content: center;
+
   align-items: center;
 `;
